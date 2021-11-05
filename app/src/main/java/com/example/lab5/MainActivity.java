9683264e1d1eaa464af21e174e1cff590a003689
karolusmax <kmaksymiuk5555@gmail.com>
2021-11-05 19:17:00 +0100

    features added

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        FloatingActionButton fab = findViewById(R.id.action_settings);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
